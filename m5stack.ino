7a597201bc751419d0d123a754b3350d0866a26c
BrownieAlice <info@browniealice.net>
2018-12-04 11:20:31 +0900

    add music

--- m5stack.ino
+++ m5stack.ino
@@ -1,39 +1,53 @@
 #include <M5Stack.h>
+#include <WiFi.h>
+#include "AudioFileSourceSD.h"
+#include "AudioFileSourceID3.h"
+#include "AudioGeneratorMP3.h"
+#include "AudioOutputI2S.h"
+
+AudioGeneratorMP3 *mp3;
+AudioFileSourceSD *file;
+AudioOutputI2S *out;
+AudioFileSourceID3 *id3;
+const unsigned int size = 7;
+char* list[size] = {"/uzuho.mp3", "/fran.mp3", "/hina.mp3", "/koishi.mp3", "/okina.mp3", "/remilia.mp3", "/sakuya.mp3"};
+unsigned int pos = 3;
 
 // the setup routine runs once when M5Stack starts up
 void setup(){
 
   // Initialize the M5Stack object
-  M5.begin(true, false, true);
+  M5.begin();
+  WiFi.mode(WIFI_OFF);
+  delay(500);
 
   // LCD display
   M5.Lcd.println("Hello World");
   Serial.println("Hello World");
-  pinMode(3, OUTPUT);
-  pinMode(17, OUTPUT);
-  pinMode(16, OUTPUT);
-  digitalWrite(3, LOW);
-  digitalWrite(17, LOW);
-  digitalWrite(16, LOW);
+
+  out = new AudioOutputI2S(0, 1); // Output to builtInDAC
+  out->SetOutputModeMono(true);
+  mp3 = new AudioGeneratorMP3();
+  file = new AudioFileSourceSD(list[pos]);
+  id3 = new AudioFileSourceID3(file);
 }
 
 // the loop routine runs over and over again forever
 void loop() {
   M5.update();
-
-  if (M5.BtnA.wasReleased()) {
-      M5.Lcd.println("pressed!");
-      vTaskDelay(5000 / portTICK_RATE_MS);
-
-      digitalWrite(3, HIGH);
-      digitalWrite(17, LOW);
-      ledcSetup(3, 490, 8);
-      ledcAttachPin(16, 3);
-      ledcWrite(3, 125);
-      vTaskDelay(5000 / portTICK_RATE_MS);
-
-      digitalWrite(3, LOW);
-      digitalWrite(17, LOW);
-      digitalWrite(16, LOW);
+  if (mp3->isRunning()) {
+    if (!mp3->loop()) mp3->stop();
+  } else {
+    M5.Lcd.printf("MP3 done\n");
+    delay(1000);
+  }
+  if (M5.BtnA.wasReleased() && !mp3->isRunning()) {
+    if (size <= ++pos) {
+      pos = 0;
+    }
+    file = new AudioFileSourceSD(list[pos]);
+    id3 = new AudioFileSourceID3(file);
+    M5.Lcd.printf("Start!\n");
+    mp3->begin(id3, out);
   }
 }
