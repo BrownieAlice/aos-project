cb058dc110304383ae5ffd17be53f1cead3ca5c9
BrownieAlice <info@browniealice.net>
2018-12-04 12:46:29 +0900

    modify to runQ

--- m5stack.ino
+++ m5stack.ino
@@ -12,9 +12,23 @@ void setup(){
   pinMode(3, OUTPUT);
   pinMode(17, OUTPUT);
   pinMode(16, OUTPUT);
+  pinMode(21, OUTPUT);
+  pinMode(22, OUTPUT);
+  pinMode(23, OUTPUT);
+  pinMode(2, OUTPUT);
+  pinMode(5, OUTPUT);
+  pinMode(25, OUTPUT);
+  pinMode(26, OUTPUT);
   digitalWrite(3, LOW);
   digitalWrite(17, LOW);
   digitalWrite(16, LOW);
+  digitalWrite(21, LOW);
+  digitalWrite(22, LOW);
+  digitalWrite(23, LOW);
+  digitalWrite(2, LOW);
+  digitalWrite(5, LOW);
+  digitalWrite(25, LOW);
+  digitalWrite(26, LOW);
 }
 
 // the loop routine runs over and over again forever
@@ -27,13 +41,25 @@ void loop() {
 
       digitalWrite(3, HIGH);
       digitalWrite(17, LOW);
+      digitalWrite(21, HIGH);
+      digitalWrite(22, LOW);
+      digitalWrite(2, HIGH);
+      digitalWrite(5, LOW);
+      digitalWrite(26, HIGH);
+
       ledcSetup(3, 490, 8);
       ledcAttachPin(16, 3);
-      ledcWrite(3, 125);
+      ledcAttachPin(23, 3);
+      ledcAttachPin(25, 3);
+      ledcWrite(3, 100);
       vTaskDelay(5000 / portTICK_RATE_MS);
 
       digitalWrite(3, LOW);
       digitalWrite(17, LOW);
-      digitalWrite(16, LOW);
+      digitalWrite(21, LOW);
+      digitalWrite(22, LOW);
+      digitalWrite(2, LOW);
+      digitalWrite(5, LOW);
+      digitalWrite(26, LOW);
   }
 }
